7f930c32fd3e574400e40a22efa139ba51cea813
Gautam Jha <Gautam.jha911@gmail.com>
2020-10-17 00:20:34 +0530

    updated the codes

--- app.py
+++ app.py
@@ -19,4 +19,14 @@ api.add_resource(HelloWorld,'/')
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+    
+api.add_resource(HelloWorld,'/')
+
+class hel(Resource):
+    def get(self):
+        return {'hello':'world'}
+
+api.add_resource(HelloWorld,'/')
 
